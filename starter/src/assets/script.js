de50c669c020124f4f676d6f6e2882c180392251
Your-崔吉宏 <your-1136227302@qq.com>
2023-09-24 22:21:41 -0400

    first

--- starter/src/assets/script.js
+++ starter/src/assets/script.js
@@ -1,5 +1,5 @@
 /* Create an array named products which you will use to add all of your product object literals that you create in the next step. */
-
+let products = ["cherry","orange","stawberry"]
 /* Create 3 or more product objects using object literal notation 
    Each product should include five properties
    - name: name of product (string)
@@ -8,7 +8,27 @@
    - productId: unique id for the product (number)
    - image: picture of product (url string)
 */
-
+const cherry = {
+  name:"cherry",
+  price:1,
+  quantity:0,
+  productId:4001,
+  image:"./image/cherry.jpg"
+}
+const orange = {
+  name:"orange",
+  price:2,
+  quantity:0,
+  productId:4002,
+  image:"./image/orange.jpg"
+}
+const stawberry = {
+  name:"stawberry",
+  price:3,
+  quantity:0,
+  productId:4003,
+  image:"./image/stawberry.jpg"
+}
 /* Images provided in /images folder. All images from Unsplash.com
    - cherry.jpg by Mae Mu
    - orange.jpg by Mae Mu
@@ -16,41 +36,106 @@
 */
 
 /* Declare an empty array named cart to hold the items in the cart */
-
+let cart = []
 /* Create a function named addProductToCart that takes in the product productId as an argument
   - addProductToCart should get the correct product based on the productId
   - addProductToCart should then increase the product's quantity
   - if the product is not already in the cart, add it to the cart
 */
+function addProductToCart(productId) {
+  let theproduct
+  if (productId === cherry.productId) {
+    theproduct = cherry
+  }else if (productId === orange.productId){
+    theproduct = orange
+  }else if (productId === stawberry.productId){
+    theproduct = stawberry
+  }
 
+  increaseQuantity(productId)
+  if (cart.indexOf(theproduct) === -1) {
+    cart.push(theproduct)
+}
 /* Create a function named increaseQuantity that takes in the productId as an argument
   - increaseQuantity should get the correct product based on the productId
   - increaseQuantity should then increase the product's quantity
 */
+function increaseQuantity(productId) {
+  let theproduct
+  if (productId === cherry.productId) {
+    theproduct = cherry
+  }else if (productId === orange.productId){
+    theproduct = orange
+  }else if (productId === stawberry.productId){
+    theproduct = stawberry
+  }
+
+  theproduct.quantity += 1
+}
 
 /* Create a function named decreaseQuantity that takes in the productId as an argument
   - decreaseQuantity should get the correct product based on the productId
   - decreaseQuantity should decrease the quantity of the product
   - if the function decreases the quantity to 0, the product is removed from the cart
 */
+function decreaseQuantity(productId) {
+  let theproduct
+  if (productId === cherry.productId) {
+    theproduct = cherry
+  }else if (productId === orange.productId){
+    theproduct = orange
+  }else if (productId === stawberry.productId){
+    theproduct = stawberry
+  }
+
+  theproduct.quantity -= 1
+  if (theproduct.quantity === 0) {
+    removeProductFromCart(productId)
+  }
+}
 
 /* Create a function named removeProductFromCart that takes in the productId as an argument
   - removeProductFromCart should get the correct product based on the productId
   - removeProductFromCart should update the product quantity to 0
   - removeProductFromCart should remove the product from the cart
 */
+function removeProductFromCart(productId) {
+  let theproduct
+  if (productId === cherry.productId) {
+    theproduct = cherry
+  }else if (productId === orange.productId){
+    theproduct = orange
+  }else if (productId === stawberry.productId){
+    theproduct = stawberry
+  }
+
+  theproduct.quantity === 0
+  cart = cart.filter(item => item !== theproduct);
+}
 
 /* Create a function named cartTotal that has no parameters
   - cartTotal should iterate through the cart to get the total of all products
   - cartTotal should return the sum of the products in the cart
 */
+function cartTotal() {
+  let totalprice
+  for (let index = 0; index < cart.length; index++) {
+    totalprice += cart[index].quantity * cart[index].price;
+  }
+  return totalprice
+}
 
 /* Create a function called emptyCart that empties the products from the cart */
-
+function emptyCart() {
+  cart.splice(0, cart.length)
+}
 /* Create a function named pay that takes in an amount as an argument
   - pay will return a negative number if there is a remaining balance
   - pay will return a positive number if money should be returned to customer
 */
+function pay(amount) {
+  return Number(amount - cartTotal())
+}
 
 /* Place stand out suggestions here (stand out suggestions can be found at the bottom of the project rubric.)*/
 
